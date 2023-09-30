d6b3cf2183a1892344111c7103d5fff1a66dea11
Aleksej Pasechnik <arhellist@mail.ru>
2023-09-30 23:34:47 +0400

    Test-bed

---
+++ src/js/__tests__/tooltip-wiget.test.js
@@ -0,0 +1,16 @@
+import { Tooltip } from "../tooltip/tooltip-wiget";
+
+test("tooltip-wiget", () => {
+  const tooltip = document.querySelector('body').insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class='tooltip'>
+      <p class='tooltip-header'>Заголовок поповера</p>
+      <p class='tooltip-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam perspiciatis error libero obcaecati nemo natus assumenda facilis quae quod veritatis! Sapiente accusantium quibusdam vitae blanditiis id aspernatur vero similique facilis.</p>
+      </div>
+      `
+  );
+  const addTooltip = new Tooltip();
+
+  expect(tooltip).toEqual(addTooltip.addTooltip());
+});
