c5f38dcdadd69cad055f303aa83f8345b843fc39
Aleksej Pasechnik <arhellist@mail.ru>
2023-10-02 13:52:09 +0400

    lint-good

--- src/js/__tests__/tooltip-wiget.test.js
+++ src/js/__tests__/tooltip-wiget.test.js
@@ -1,14 +1,14 @@
-import { Tooltip } from "../tooltip/tooltip-wiget";
+import Tooltip from '../tooltip/tooltip-wiget';
 
-test("tooltip-wiget", () => {
+test('tooltip-wiget', () => {
   const tooltip = document.querySelector('body').insertAdjacentHTML(
-    "beforeend",
+    'beforeend',
     `
       <div class='tooltip'>
       <p class='tooltip-header'>Заголовок поповера</p>
       <p class='tooltip-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam perspiciatis error libero obcaecati nemo natus assumenda facilis quae quod veritatis! Sapiente accusantium quibusdam vitae blanditiis id aspernatur vero similique facilis.</p>
       </div>
-      `
+      `,
   );
   const addTooltip = new Tooltip();
 
