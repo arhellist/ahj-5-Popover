f3846dec9bcc6775fdfbabdad133058daba6fad3
Aleksej Pasechnik <arhellist@mail.ru>
2023-10-02 17:52:26 +0400

    ready

--- src/js/__tests__/tooltip-wiget.test.js
+++ src/js/__tests__/tooltip-wiget.test.js
@@ -1,16 +1,28 @@
-import Tooltip from '../tooltip/tooltip-wiget';
+import Tooltip from "../tooltip/tooltip-wiget";
 
-test('tooltip-wiget', () => {
-  const tooltip = document.querySelector('body').insertAdjacentHTML(
-    'beforeend',
+test("tooltip-wiget", () => {
+  const tooltip = document.querySelector("body").insertAdjacentHTML(
+    "beforeend",
     `
+    <button
+      class="glowing-btn"
+      data-header="Заголовок поповера"
+      data-content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam perspiciatis error libero obcaecati nemo natus assumenda facilis quae quod veritatis! Sapiente accusantium quibusdam vitae blanditiis id aspernatur vero similique facilis."
+    >
+      <span class="glowing-txt"
+        >PO<span class="faulty-letter">PO</span>VER</span
+      >
+    </button>
       <div class='tooltip'>
       <p class='tooltip-header'>Заголовок поповера</p>
       <p class='tooltip-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam perspiciatis error libero obcaecati nemo natus assumenda facilis quae quod veritatis! Sapiente accusantium quibusdam vitae blanditiis id aspernatur vero similique facilis.</p>
       </div>
-      `,
+      `
   );
-  const addTooltip = new Tooltip();
+  console.log(document.querySelector("body").innerHTML);
+  const button = document.querySelector(".glowing-btn");
+  const body = document.querySelector("body");
+  const addTooltip = new Tooltip(button, body);
 
   expect(tooltip).toEqual(addTooltip.addTooltip());
 });
--- src/js/tooltip/tooltip-wiget.js
+++ src/js/tooltip/tooltip-wiget.js
@@ -1,30 +1,32 @@
 export default class Tooltip {
-  constructor() {
+  constructor(button, body) {
     this.button = document.querySelector('.glowing-btn');
     this.body = document.querySelector('body');
   }
 
   addTooltip() {
     this.body.insertAdjacentHTML(
-      'beforeend',
+      "beforeend",
       `
     <div class='tooltip'>
     <p class='tooltip-header'>${this.button.dataset.header}</p>
     <p class='tooltip-text'>${this.button.dataset.content}</p>
     </div>
-    `,
+    `
     );
   }
 
   clickPopover() {
-    this.button.addEventListener('click', () => {
-      if (document.querySelector('.tooltip') === null) {
+    this.button.addEventListener("click", () => {
+      if (document.querySelector(".tooltip") === null) {
         this.addTooltip();
-        const { left, top } = document.querySelector('.tooltip').getBoundingClientRect();// находим коордираны всплывающего окна
-        document.querySelector('.tooltip').style.left = `${left}px`; // позиционируем левую сторону относительно левого края страницы
-        document.querySelector('.tooltip').style.bottom = `${top + 140}px`; // позиционируем нижнюю сторону относительно верхнего края края всплывающего окна (исходного полложения)
-      } else if (document.querySelector('.tooltip') !== null) {
-        document.querySelector('.tooltip').remove();
+        const { left, top } = document
+          .querySelector(".tooltip")
+          .getBoundingClientRect(); // находим коордираны всплывающего окна
+        document.querySelector(".tooltip").style.left = `${left}px`; // позиционируем левую сторону относительно левого края страницы
+        document.querySelector(".tooltip").style.bottom = `${top + 140}px`; // позиционируем нижнюю сторону относительно верхнего края края всплывающего окна (исходного полложения)
+      } else if (document.querySelector(".tooltip") !== null) {
+        document.querySelector(".tooltip").remove();
       }
     });
   }
