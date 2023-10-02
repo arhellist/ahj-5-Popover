54ad9ade6bb392917cc7e0855a313a083fa0e14c
Aleksej Pasechnik <arhellist@mail.ru>
2023-10-02 17:55:39 +0400

    ready

--- src/js/__tests__/tooltip-wiget.test.js
+++ src/js/__tests__/tooltip-wiget.test.js
@@ -1,8 +1,8 @@
-import Tooltip from "../tooltip/tooltip-wiget";
+import Tooltip from '../tooltip/tooltip-wiget';
 
-test("tooltip-wiget", () => {
-  const tooltip = document.querySelector("body").insertAdjacentHTML(
-    "beforeend",
+test('tooltip-wiget', () => {
+  const tooltip = document.querySelector('body').insertAdjacentHTML(
+    'beforeend',
     `
     <button
       class="glowing-btn"
@@ -17,11 +17,10 @@ test("tooltip-wiget", () => {
       <p class='tooltip-header'>Заголовок поповера</p>
       <p class='tooltip-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam perspiciatis error libero obcaecati nemo natus assumenda facilis quae quod veritatis! Sapiente accusantium quibusdam vitae blanditiis id aspernatur vero similique facilis.</p>
       </div>
-      `
+      `,
   );
-  console.log(document.querySelector("body").innerHTML);
-  const button = document.querySelector(".glowing-btn");
-  const body = document.querySelector("body");
+  const button = document.querySelector('.glowing-btn');
+  const body = document.querySelector('body');
   const addTooltip = new Tooltip(button, body);
 
   expect(tooltip).toEqual(addTooltip.addTooltip());
