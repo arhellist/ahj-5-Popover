f11f6c6b79decaa91bc7265b2cfa3645fd15a5d4
Aleksej Pasechnik <arhellist@mail.ru>
2023-10-06 12:32:58 +0400

    begin

--- src/js/tooltip/tooltip-wiget.js
+++ src/js/tooltip/tooltip-wiget.js
@@ -5,29 +5,28 @@ export default class Tooltip {
   }
 
   addTooltip() {
-    this.body.insertAdjacentHTML(
+    const tooltip = this.button.insertAdjacentHTML(
       'beforeend',
       `
     <div class='tooltip'>
     <p class='tooltip-header'>${this.button.dataset.header}</p>
     <p class='tooltip-text'>${this.button.dataset.content}</p>
     </div>
-    `,
+    `
     );
+   
   }
 
-  clickPopover() {
-    this.button.addEventListener('click', () => {
-      if (document.querySelector('.tooltip') === null) {
-        this.addTooltip();
-        const { left, top } = document
-          .querySelector('.tooltip')
-          .getBoundingClientRect(); // находим коордираны всплывающего окна
-        document.querySelector('.tooltip').style.left = `${left}px`; // позиционируем левую сторону относительно левого края страницы
-        document.querySelector('.tooltip').style.bottom = `${top + 140}px`; // позиционируем нижнюю сторону относительно верхнего края края всплывающего окна (исходного полложения)
-      } else if (document.querySelector('.tooltip') !== null) {
-        document.querySelector('.tooltip').remove();
-      }
-    });
+  clickPopover(){
+    this.button.addEventListener('click', ()=>{
+if(document.querySelector('.tooltip') === null){
+  this.addTooltip()
+  document.querySelector('.tooltip').style.bottom = 100 + 'px' ; 
+  document.querySelector('.tooltip').style.left = 68 + 'px' ;
+}
+else if (document.querySelector('.tooltip') !== null) {
+  document.querySelector('.tooltip').remove();
+}
+    })
   }
 }
